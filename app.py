720868b8534c708fe61c3f8fa61750a556b98b93
matheus <matheuscluiz@gmail.com>
2024-11-16 18:09:47 -0300

    Adição dos input do usuario

--- app.py
+++ app.py
@@ -10,12 +10,6 @@ import pickle
 
 print("Abriu a pagina")
 
-# verifica se a senha de acesso está correta
-if not util.check_password():
-    # se a senha estiver errada, para o processamento do app
-    print("Usuario nao logado")
-    st.stop()
-    
 
 print("Carregou a pagina")
 
@@ -25,7 +19,7 @@ print("Carregou a pagina")
 dados = data_handler.load_data()
 
 # carrega o modelo de predição já treinado e validado
-model = pickle.load(open('./models/final_classification_model.pkl', 'rb'))   
+model = pickle.load(open('./models/final_classification_model_melbourne.pkl', 'rb'))
 
 # começa a estrutura da interface do sistema
 st.title('Melbourne Housing ML')
@@ -35,14 +29,14 @@ data_analyses_on = st.toggle('Exibir análise dos dados')
 if data_analyses_on:
     # essa parte é só um exmplo de que é possível realizar diversas visualizações e plotagens com o streamlit
     st.header('Melbourne Housing - Dataframe')
-    
+
     # exibe todo o dataframe dos dados
     st.dataframe(dados)
 
     # plota um gráfico de barras com a contagem dos dados
-    st.header('Income')
-    st.bar_chart(dados.income.value_counts())
-    
+    st.header('Mapa de imóveis')
+    st.map(data=dados.dropna(), latitude="Lattitude", longitude="Longtitude")
+
 # daqui em diante vamos montar a inteface para capturar os dados de input do usuário para realizar a predição
 # que vai identificar predizer a renda de uma pessoa
 st.header('Preditor de income')
@@ -56,28 +50,28 @@ st.header('Preditor de income')
 # assim, todas essas informações também devem ser passadas para o modelo realizar a predição
 
 # define a linha 1 de inputs com 3 colunas
-col1, col2, col3 = st.columns(3)
+col1, col2, col3, col4, col5 = st.columns(5)
 
 # captura a idade da pessoa, como o step é 1, ele considera a idade como inteira
 with col1:
-    age = st.number_input('Age in years', step=1)
+    rooms = st.number_input('Número de cômodos', step=1)
 
 # captura os anos investidos em educação da pessoa
 with col2:
-    education_num = st.number_input('Education years', step=1)
-    
+    bathroom = st.number_input('Número de banheiros', step=1)
+
 # captura a horas trabalhadas por semana
 with col3:
-    hours_per_week = st.number_input('Hours per week', step=1)
-    
-col1, col2 = st.columns(2)
-with col1:
-    option = st.selectbox(
-        "Qual a workclass",
-        ("State-gov", "Private", "Federal-gov"),
-    )
+    buildArea = st.number_input('Área construção', step=1)
 
-submit = st.button('Predizer income')
+with col4:
+    suburb = st.text_input('Bairro')
+
+with col5:
+    bedrooms = st.number_input('Número de quartos', step=1)
+
+
+submit = st.button('Predizer valor do imóvel')
 
 # data mapping
 # essa parte do código realiza o mapeamento dos campos
@@ -85,33 +79,32 @@ submit = st.button('Predizer income')
 # assim, isso também deve ser feito aqui para haver compatibilidade nos dados
 
 # armazena todos os dados da pessoa nesse dict
-person = {}
+house = {}
 
-    
+
 # verifica se o botão submit foi pressionado
 if submit:
     # seta todos os attrs da pessoa e já realiza o mapeamento dos attrs
     # se houver atributos não numéricos, agora é o momento de realizar o mapeamento
-    person = {
-        'age': age,
-        'education-num': education_num,
-        'hour-per-week': hours_per_week,
+    house = {
+        'Rooms': rooms,
+        'Bathroom': bathroom,
+        'BuildingArea': buildArea,
+        'Suburb': suburb,
+        'Bedroom2': bedrooms,
     }
-    print(person)
-    
+    print(house)
+
     # converte a pessoa para um pandas dataframe
     # isso é feito para igualar ao tipo de dado que foi utilizado para treinar o modelo
-    values = pd.DataFrame([person])
-    print(values) 
+    values = pd.DataFrame([house])
+    print(values)
 
     # realiza a predição de income da pessoa com base nos dados inseridos pelo usuário
     results = model.predict(values)
     print(results)
-    
+
     # o modelo foi treinado para retornar uma lista com <=50k e >50k, onde cada posição da lista indica a renda da pessoa
     # como estamos realizando a predição de somente uma pessoa por vez, o modelo deverá retornar somente um elemento na lista
     if len(results) == 1:
         st.subheader(results[0])
-    
-
-    
