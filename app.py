6c61918fe7b7ec7b081b9a0b320176d4994273a0
Guilherme Schmitt <guilhermehschmitt@hotmail.com>
2024-11-21 17:44:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ dados = data_handler.load_data()
 dados = dados.dropna()
 
 # carrega o modelo de predição já treinado e validado
-model = pickle.load(open('./models/final_classification_model_melbourne_suburb.pkl', 'rb'))
+model_suburb = pickle.load(open('./models/final_classification_model_melbourne_suburb.pkl', 'rb'))
+model_region = pickle.load(open('./models/final_classification_model_melbourne_region.pkl', 'rb'))
 
 
 # calculo do suburbio com maior preço medio
@@ -47,10 +48,14 @@ suburb_aw = 'Airport West'
 preco_medio_aw = dados[dados['Suburb'] == suburb_aw]['Price'].mean()
 
 #declara label encoder para padronização dos dados
-label_encoder = LabelEncoder()
+label_encoder1 = LabelEncoder()
+label_encoder2 = LabelEncoder()
 
 #padroniza a coluna Suburb para numerico
-dados['Suburb'] = label_encoder.fit_transform(dados['Suburb'])
+dados_suburb = dados
+dados_region = dados
+dados_suburb['Suburb'] = label_encoder1.fit_transform(dados_suburb['Suburb'])
+dados_region['Regionname'] = label_encoder2.fit_transform(dados_region['Regionname'])
 
 # começa a estrutura da interface do sistema
 st.title('Melbourne Housing ML')
@@ -104,19 +109,19 @@ st.header('Preditor de imóveis')
 # essas foram as informações utilizadas para treinar o modelo
 # assim, todas essas informações também devem ser passadas para o modelo realizar a predição
 
-# Criando o formulário
-with st.form(key='formulario'):
+# Criando o formulário 1
+with st.form(key='formulario1'):
     # Usando colunas para alinhar os campos
     col1, col2 = st.columns(2)  # A coluna 2 será maior
     # Campos do formulário
     with col1:
-        rooms = st.number_input("Número de cômodos:", min_value=1, key='rooms')
+        rooms = st.number_input("Número de cômodos:", min_value=1, key='rooms_1')
     with col2:
-        bathroom = st.number_input("Número de banheiros:", min_value=1, key='bathroom')
+        bathroom = st.number_input("Número de banheiros:", min_value=1, key='bathroom_1')
     with col1:
-        build_area = st.number_input("Área construção:", min_value=1, key='build_area')
+        build_area = st.number_input("Área construção:", min_value=1, key='build_area_1')
     with col2:
-        bedrooms = st.number_input("Número de quartos:", min_value=1, key='bedrooms')
+        bedrooms = st.number_input("Número de quartos:", min_value=1, key='bedrooms_1')
 
 
     # Validação dos dados
@@ -132,7 +137,7 @@ with st.form(key='formulario'):
         # define a linha 1 de inputs com 1 colunas
         col1, col2 = st.columns(2)
         with col1:
-            region = st.selectbox(
+            suburb = st.selectbox(
                 "Suburbio",
                 ('Abbotsford', 'Aberfeldie', 'Airport West', 'Albanvale', 'Albert Park',
                 'Albion', 'Alphington', 'Altona', 'Altona Meadows', 'Altona North', 'Ardeer',
@@ -156,12 +161,50 @@ with st.form(key='formulario'):
                 'Deepdene', 'Deer Park', 'Delahey', 'Derrimut', 'Diamond Creek',
                 'Diggers Rest', 'Dingley Village', 'Doncaster', 'Doncaster East', 'Donvale',
                 'Doreen', 'Doveton', 'Eaglemont', 'East Melbourne', 'Edithvale', 'Elsternwick',
-                'Eltham', 'Eltham North', 'Elwood', 'Emerald', 'Endeavour Hills', 'Epping', 'Kurunjang', 'Kooyong', 'Notting Hill')
+                'Eltham', 'Eltham North', 'Elwood', 'Emerald', 'Endeavour Hills', 'Epping', 
+                'Kurunjang', 'Kooyong', 'Notting Hill')
             )
 
     # Botão de envio, habilitado apenas se todos os dados forem válidos
-    submit = st.form_submit_button("Predizer valor do imóvel", disabled=not st.session_state.form_valid)
+    submit_suburb = st.form_submit_button("Predizer valor do imóvel", disabled=not st.session_state.form_valid)
 
+# Criando o formulário 2
+with st.form(key='formulario2'):
+    # Usando colunas para alinhar os campos
+    col1, col2 = st.columns(2)  # A coluna 2 será maior
+    # Campos do formulário
+    with col1:
+        rooms = st.number_input("Número de cômodos:", min_value=1, key='rooms_2')
+    with col2:
+        bathroom = st.number_input("Número de banheiros:", min_value=1, key='bathroom_2')
+    with col1:
+        build_area = st.number_input("Área construção:", min_value=1, key='build_area_2')
+    with col2:
+        bedrooms = st.number_input("Número de quartos:", min_value=1, key='bedrooms_2')
+
+
+    # Validação dos dados
+    valid_rooms = rooms >= 1
+    valid_bathroom = rooms >= 1
+    valid_build_area = rooms >= 1
+    valid_bedrooms = rooms >= 1
+
+    # Armazenando a validação no session_state
+    st.session_state.form_valid = valid_rooms and valid_bathroom and valid_build_area and valid_bedrooms
+
+    with st.container():
+        # define a linha 1 de inputs com 1 colunas
+        col1, col2 = st.columns(2)
+        with col1:
+            region = st.selectbox(
+                "Região",
+                ('Eastern Metropolitan', 'Eastern Victoria', 'Northern Metropolitan',
+                'Northern Victoria', 'South-Eastern Metropolitan', 'Southern Metropolitan',
+                'Western Metropolitan', 'Western Victoria')
+            )
+
+    # Botão de envio, habilitado apenas se todos os dados forem válidos
+    submit_region = st.form_submit_button("Predizer valor do imóvel", disabled=not st.session_state.form_valid)
 
 # data mapping
 # essa parte do código realiza o mapeamento dos campos
@@ -176,14 +219,14 @@ bathroom = float(bathroom)
 bedrooms = float(bedrooms)
 rooms = float(rooms)
 
-# verifica se o botão submit foi pressionado
-if submit:
+# verifica se o botão submit foi pressionado em suburbio
+if submit_suburb:
     # se houver atributos não numéricos, realiza o mapeamento
-    encoded_region = label_encoder.transform([region])[0]
+    encoded_suburb = label_encoder1.transform([suburb])[0]
     house = {
         'BuildingArea': build_area,
         'Bathroom': bathroom,
-        'Suburb': encoded_region,
+        'Suburb': encoded_suburb,
         'Bedroom2': bedrooms,
         'Rooms': rooms,
     }
@@ -194,7 +237,31 @@ if submit:
     print(values)
 
     # realiza a predição de um valor de imovel com base nos dados inseridos pelo usuário
-    results = model.predict(values)
+    results = model_suburb.predict(values)
+    print(results)
+
+    if len(results) == 1:
+        st.subheader(f"Valor: ${results[0]:,.2f}")
+
+# verifica se o botão submit foi pressionado em regioes
+if submit_region:
+    # se houver atributos não numéricos, realiza o mapeamento
+    encoded_region = label_encoder2.transform([region])[0]
+    house = {
+        'BuildingArea': build_area,
+        'Bathroom': bathroom,
+        'Regionname': encoded_region,
+        'Bedroom2': bedrooms,
+        'Rooms': rooms,
+    }
+    print(house)
+
+    # isso é feito para igualar ao tipo de dado que foi utilizado para treinar o modelo
+    values = pd.DataFrame([house])
+    print(values)
+
+    # realiza a predição de um valor de imovel com base nos dados inseridos pelo usuário
+    results = model_region.predict(values)
     print(results)
 
     if len(results) == 1:
